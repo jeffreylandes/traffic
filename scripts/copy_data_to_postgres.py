4048b3b43ccb5abb80d44c56e10fb0cf8e6bf152
Jeffrey <jeffrey@spacemaker.ai>
2021-10-13 21:57:59 -0400

    fix: unique osm id

--- scripts/copy_data_to_postgres.py
+++ scripts/copy_data_to_postgres.py
@@ -8,15 +8,9 @@ def main():
 
     data = gpd.read_file(PROCESSED_MERGED_PATH)
 
-    # TODO: Should avoid this by cleaning up feature names early in the data processing workflow
-    final_data = data.copy()
-    final_data['adt'] = final_data['ADT'].copy()
-    del final_data['ADT']
-    del final_data['road_tag']
-
     engine = create_engine("postgresql://postgres:postgres@localhost/traffic")
 
-    final_data.to_postgis(name="traffic", con=engine, if_exists="append")
+    data.to_postgis(name="traffic", con=engine, if_exists="append")
 
 
 if __name__ == "__main__":
--- scripts/rejoin_osm.py
+++ scripts/rejoin_osm.py
@@ -1,4 +1,6 @@
 import geopandas as gpd
+import numpy as np
+
 from constants import (
     ROAD_TAG_FEATURE_NAME,
     OSM_GEOJSON_PATH,
@@ -9,8 +11,14 @@ from constants import (
 
 FINAL_COLUMNS = {
     "id": "osm_id",
-    "CURRENT_VO": "ADT",
+    "CURRENT_VO": "adt",
 }
+
+
+def get_mean_adt(df: gpd.GeoDataFrame):
+    mean_adt = np.mean(df['adt'])
+    df.iloc[0]["adt"] = mean_adt
+    return df.iloc[0]
 
 
 def main():
@@ -20,8 +28,13 @@ def main():
     merged_roads = original_osm_roads.merge(traffic_counts, how="left", on="id")
     merged_roads["geometry"] = merged_roads["geometry_x"]
     merged_roads.rename(columns=FINAL_COLUMNS, inplace=True)
-    final_table = merged_roads[["osm_id", "ADT", "geometry", ROAD_TAG_FEATURE_NAME]]
-    gpd.GeoDataFrame(final_table).to_file(PROCESSED_MERGED_PATH)
+    selected_columns = merged_roads[["osm_id", "adt", "geometry", ROAD_TAG_FEATURE_NAME]]
+    selected_columns.groupby("osm_id")
+    mean_adt_per_osm = selected_columns.groupby("osm_id").apply(
+        lambda osm_id: get_mean_adt(osm_id)
+    )
+    mean_adt_per_osm = mean_adt_per_osm[selected_columns.columns]
+    gpd.GeoDataFrame(mean_adt_per_osm).to_file(PROCESSED_MERGED_PATH, index=False)
 
 
 if __name__ == "__main__":
